b1c73916f99590187c512974f70907fce9ed09f1
Florian Herzog <fl.herzog@icloud.com>
2020-11-19 14:50:48 +0100

    #3 🚨 Removed deprecation notice

--- app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/ui/main/MainFragment.kt
+++ app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import de.uni.hannover.hci.mi.team6.covidcheckin.R
 
 class MainFragment : Fragment() {
@@ -14,7 +15,7 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -23,7 +24,6 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
