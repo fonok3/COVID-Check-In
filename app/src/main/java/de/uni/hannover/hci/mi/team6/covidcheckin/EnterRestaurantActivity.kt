c215a9c259733c0405aea5a1feaab16db5a91746
Elias <eliasaaronbaalmann@gmx.de>
2020-11-23 00:03:02 +0100

    Added enter_restaurant_activity and tested notifications (can be found in the new activity but will have to be moved from there). Also added a Singleton that stores the Personal Data of the User. This singleton is currently (de-)serialized in the new activity, this should be done somewhere else (e.g. the main activity though)

---
+++ app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/EnterRestaurantActivity.kt
@@ -0,0 +1,133 @@
+package de.uni.hannover.hci.mi.team6.covidcheckin
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import java.io.File
+
+/**
+ * Activity that shows the screen where the User can accept or deny transfer of his UserData to the restaurant
+ */
+class EnterRestaurantActivity : AppCompatActivity() {
+    lateinit var userPersonalDataView: TextView
+    lateinit var yesButton: Button
+    lateinit var noButton: Button
+
+    /**
+     * File where UserData is persisted
+     */
+    private val userDataFileName = "userData.txt" //TODO move to Start Activity
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //TODO move to Start Activity
+        val file = File(applicationContext.filesDir, userDataFileName)
+        if (file.exists()) {
+            val userData = file.readLines()
+            UserPersonalDataSingleton.set(
+                userData[0],
+                userData[1],
+                userData[2],
+                userData[3],
+                userData[4],
+                userData[5],
+                userData[6]
+            )
+        }
+        UserPersonalDataSingleton.observers.add { ->
+            file.writeText(
+                "${UserPersonalDataSingleton.firstName}\n" +
+                        "${UserPersonalDataSingleton.lastName}\n" +
+                        "${UserPersonalDataSingleton.street}\n" +
+                        "${UserPersonalDataSingleton.streetNumber}\n" +
+                        "${UserPersonalDataSingleton.zipCode}\n" +
+                        "${UserPersonalDataSingleton.city}\n" +
+                        "${UserPersonalDataSingleton.phoneNumber}"
+            )
+        }
+
+        setContentView(R.layout.enter_restaurant_activity)
+
+        userPersonalDataView = findViewById(R.id.userPersonalDataView)
+        yesButton = findViewById(R.id.yes_button)
+        noButton = findViewById(R.id.no_button)
+
+        yesButton.setOnClickListener { notificationTest() } //TODO Open correct Activity
+        yesButton.setOnClickListener { notificationTest() } //TODO Open correct Activity
+
+        updateUserPersonalData()
+        UserPersonalDataSingleton.observers.add { -> updateUserPersonalData() }
+    }
+
+    private fun updateUserPersonalData() {
+        userPersonalDataView.setText(UserPersonalDataSingleton.toString())
+    }
+
+
+    //TODO move all this notification stuff
+    private val CHANNEL_ID = "channel_id_example_01"
+    private val notificationId = 101
+
+    /**
+     * GEHÖRT HIER NICHT HIN, IST NUR ZUM AUSPROBIEREN BEVOR DIE NOTIFICATION AN DER RICHTIGEN STELLE ERSTELLT WIRD
+     */
+    private fun notificationTest() {
+        createNotificationChannel()
+
+        val intent =
+            Intent(this, MainActivity::class.java).apply { //TODO Link zur richtigen Activity
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+
+            .setSmallIcon(R.mipmap.ic_launcher_foreground)
+            .setContentTitle("Restaurant betreten")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("Du hast das Restaurant \"Muster\" betreten. Möchtest du automatisch deine Daten übertragen?")//TODO Dynamisch den Restaurantnamen übernehmen
+            )
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(notificationId, builder.build())
+        }
+
+    }
+
+    /**
+     * GEHÖRT HIER NICHT HIN, IST NUR ZUM AUSPROBIEREN BEVOR DIE NOTIFICATION AN DER RICHTIGEN STELLE ERSTELLT WIRD
+     */
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+}
