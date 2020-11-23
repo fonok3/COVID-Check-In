c25e401bb88513fdb9875bfde19fe9760416e37f
Elias <eliasaaronbaalmann@gmx.de>
2020-11-23 15:37:01 +0100

    removed mainfragment (is it needed in any way?) and added button to test EnterRestaurantActivity.

--- app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/MainActivity.kt
+++ app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/MainActivity.kt
@@ -1,18 +1,19 @@
 package de.uni.hannover.hci.mi.team6.covidcheckin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import de.uni.hannover.hci.mi.team6.covidcheckin.ui.main.MainFragment
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
     }
+
+    fun openRestaurantActivity(v: View) {
+        startActivity(Intent(this, EnterRestaurantActivity::class.java))
+    }
+
 }
