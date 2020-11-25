632f82a6d6767b068c85084f41112c6e22072e55
Florian Herzog <fl.herzog@icloud.com>
2020-11-25 18:16:22 +0100

    #3 ✨ Added permissions stuff

--- app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/permission/repository/AndroidPermissionsRepository.kt
+++ app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/permission/repository/AndroidPermissionsRepository.kt
@@ -3,9 +3,12 @@ package de.uni.hannover.hci.mi.team6.covidcheckin.permission.repository
 import android.Manifest
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 
-class AndroidPermissionsRepository(private val activity: Activity): PermissionsRepository {
+class AndroidPermissionsRepository(private val activity: Activity) : PermissionsRepository {
     companion object {
         const val TAG: String = "AndroidPermissionsRepository"
         const val REQUEST_ENABLE_PERMISSIONS: Int = 1
@@ -17,35 +20,48 @@ class AndroidPermissionsRepository(private val activity: Activity): PermissionsR
     override val isBluetoothEnabled: Boolean
         get() = bluetoothAdapter?.isEnabled == true
     override val isLocationEnabled: Boolean
-        get() = TODO("not implemented")
+        get() = ContextCompat.checkSelfPermission(
+            activity.applicationContext,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(
+            activity.applicationContext,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
     override val areNotificationsEnabled: Boolean
-        get() = TODO("not implemented")
+        get() = NotificationManagerCompat.from(activity.applicationContext).areNotificationsEnabled()
 
     override fun enableBluetooth() {
         if (!isBluetoothEnabled) {
             return
         }
-        ActivityCompat.requestPermissions(activity,
+        ActivityCompat.requestPermissions(
+            activity,
             arrayOf(Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN),
-            AndroidPermissionsRepository.REQUEST_ENABLE_PERMISSIONS)
+            REQUEST_ENABLE_PERMISSIONS
+        )
     }
 
     override fun enableLocation() {
         if (!isLocationEnabled) {
             return
         }
-        ActivityCompat.requestPermissions(activity,
+        ActivityCompat.requestPermissions(
+            activity,
             arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-            AndroidPermissionsRepository.REQUEST_ENABLE_PERMISSIONS)
+            REQUEST_ENABLE_PERMISSIONS
+        )
     }
 
     override fun enableNotifications() {
         if (!areNotificationsEnabled) {
             return
         }
-        ActivityCompat.requestPermissions(activity,
+        ActivityCompat.requestPermissions(
+            activity,
             arrayOf(Manifest.permission.ACCESS_NOTIFICATION_POLICY),
-            AndroidPermissionsRepository.REQUEST_ENABLE_PERMISSIONS)
+            REQUEST_ENABLE_PERMISSIONS
+        )
     }
 
     override fun addPermissionsChangedListener(listener: PermissionsRepository.ChangedListener) {
--- app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/permission/ui/PermissionsFragment.kt
+++ app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/permission/ui/PermissionsFragment.kt
@@ -1,29 +1,66 @@
 package de.uni.hannover.hci.mi.team6.covidcheckin.permission.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import de.uni.hannover.hci.mi.team6.covidcheckin.R
+import de.uni.hannover.hci.mi.team6.covidcheckin.permission.repository.AndroidPermissionsRepository
+import de.uni.hannover.hci.mi.team6.covidcheckin.permission.repository.PermissionsRepository
+import kotlinx.android.synthetic.main.permissions_fragment.*
 
-class PermissionsFragment : Fragment() {
+
+class PermissionsFragment : Fragment(),
+    CompoundButton.OnCheckedChangeListener,
+    PermissionsRepository.ChangedListener {
 
     companion object {
         fun newInstance() = PermissionsFragment()
     }
 
+    private lateinit var repository: PermissionsRepository
+
     private val viewModel: PermissionsViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
+        activity?.let { repository = AndroidPermissionsRepository(it) }
+
+        notifications_switch.setOnCheckedChangeListener(this)
+        location_switch.setOnCheckedChangeListener(this)
+        bluetooth_switch.setOnCheckedChangeListener(this)
+    }
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        if (!isChecked) { return refreshSwitches() }
+
+        if (buttonView == notifications_switch)
+            repository.enableNotifications()
+
+        if (buttonView == location_switch)
+            repository.enableLocation()
+
+        if (buttonView == bluetooth_switch)
+            repository.enableBluetooth()
+    }
+
+    override fun permissionDidChange(permission: PermissionsRepository.Permission) {
+        refreshSwitches()
+    }
+
+    private fun refreshSwitches() {
+        notifications_switch.isEnabled = repository.areNotificationsEnabled
+        location_switch.isEnabled = repository.isLocationEnabled
+        bluetooth_switch.isEnabled = repository.isBluetoothEnabled
     }
 
 }
+
