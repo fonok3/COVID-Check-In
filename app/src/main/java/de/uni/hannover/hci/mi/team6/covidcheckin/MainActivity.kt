01bb874b55efce84602a09a7038b77ee0cd10bac
Quang Le <quangphipx15@gmail.com>
2020-11-26 11:39:53 +0100

    change fragment to activity

--- app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/MainActivity.kt
+++ app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/MainActivity.kt
@@ -2,17 +2,11 @@ package de.uni.hannover.hci.mi.team6.covidcheckin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import de.uni.hannover.hci.mi.team6.covidcheckin.ui.main.SuccessCheckinFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, SuccessCheckinFragment.newInstance())
-                    .commitNow()
-        }
     }
 }
