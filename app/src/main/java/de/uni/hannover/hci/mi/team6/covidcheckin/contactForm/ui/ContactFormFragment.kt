d1906bf2187bb8774de8d2e1a3f09be536c32038
Anmar Nazar Hizber <anmar_nh@outlook.com>
2020-12-02 20:19:20 +0100

    Some fixes to the ContactForm UI

--- app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/contactForm/ui/ContactFormFragment.kt
+++ app/src/main/java/de/uni/hannover/hci/mi/team6/covidcheckin/contactForm/ui/ContactFormFragment.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import de.uni.hannover.hci.mi.team6.covidcheckin.R
 import de.uni.hannover.hci.mi.team6.covidcheckin.bluetooth.BluetoothActivity
+import de.uni.hannover.hci.mi.team6.covidcheckin.contactForm.ContactFormActivity
 import de.uni.hannover.hci.mi.team6.covidcheckin.model.CustomerPersonalData
 import de.uni.hannover.hci.mi.team6.covidcheckin.services.ServicesModule
 import kotlinx.android.synthetic.visitor.fragment_contact_form.*
@@ -34,20 +37,33 @@ class ContactFormFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+
+
+
         weiter_button.setOnClickListener {
 
-            val firstName = editText_Vorname.text.toString()
-            val sectionName = this.editText_Nachname.text.toString()
-            val street = this.editText_strasse.text.toString()
-            val streetNumber = this.editText_hausnummer.text.toString()
-            val zipCode = this.editText_plz.text.toString()
-            val city = this.editText_stadt.text.toString()
-            val phoneNumber = this.editText_telefone.text.toString()
+
+            val firstName = this.editText_Vorname.text.toString().trim()
+            val secondName = this.editText_Nachname.text.toString().trim()
+            val street = this.editText_strasse.text.toString().trim()
+            val streetNumber = this.editText_hausnummer.text.toString().trim()
+            val zipCode = this.editText_plz.text.toString().trim()
+            val city = this.editText_stadt.text.toString().trim()
+            val phoneNumber = this.editText_telefone.text.toString().trim()
+
+            /**
+             * Returns if not all Data were given
+             */
+
+            if(firstName.isEmpty() or secondName.isEmpty() or street.isEmpty() or streetNumber.isEmpty() or zipCode.isEmpty() or city.isEmpty() or phoneNumber.isEmpty()){
+                Toast.makeText(activity,"Bitte Geben Sie alle Ihre Daten ein.",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
             ServicesModule.customerPersonalDataService.save(
                 CustomerPersonalData(
                     firstName,
-                    sectionName,
+                    secondName,
                     street,
                     streetNumber,
                     zipCode,
@@ -55,8 +71,19 @@ class ContactFormFragment : Fragment() {
                     phoneNumber
                 )
             )
-            val intent = Intent(activity, BluetoothActivity::class.java)
-            startActivity(intent)
+
+
+            /**
+             * To go to next Activity, the user must confirm the checkbox.
+             */
+
+            if (checkBox.isChecked()){
+                val intent = Intent(activity, BluetoothActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(activity, "Bitte akzeptieren Sie die Datenschutzerklärung.", Toast.LENGTH_SHORT).show()
+            }
 
         }
     }
