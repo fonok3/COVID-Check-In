bf0c88105f39078b4f999273d4f63a73b7d89afb
Anmar Nazar Hizber <anmar_nh@outlook.com>
2020-12-11 20:55:46 +0100

    1) PLZ gets only numbers now. 2) Textfields become Red when not filled 3) new strokes around the text fields, waiting for review.

--- app/src/visitor/java/de/uni/hannover/hci/mi/team6/covidcheckin/contactForm/ui/ContactFormFragment.kt
+++ app/src/visitor/java/de/uni/hannover/hci/mi/team6/covidcheckin/contactForm/ui/ContactFormFragment.kt
@@ -1,6 +1,8 @@
 package de.uni.hannover.hci.mi.team6.covidcheckin.contactForm.ui
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,7 +14,8 @@ import de.uni.hannover.hci.mi.team6.covidcheckin.bluetooth.BluetoothActivity
 import de.uni.hannover.hci.mi.team6.covidcheckin.model.CustomerPersonalData
 import de.uni.hannover.hci.mi.team6.covidcheckin.services.ServicesModule
 import kotlinx.android.synthetic.visitor.fragment_contact_form.*
-
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.ShapeDrawable
 
 /**
  * This fragment will be added to the activity contactFormActivity. The user will give his data as an input the the data will be
@@ -53,11 +56,98 @@ class ContactFormFragment : Fragment() {
              * Returns if not all Data were given
              */
 
+            if (firstName.isEmpty()){
+
+                var bac: GradientDrawable = editText_Vorname.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_Vorname.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if (secondName.isEmpty()){
+
+
+                var bac: GradientDrawable = editText_Nachname.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_Nachname.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if(street.isEmpty()){
+
+                var bac: GradientDrawable = editText_strasse.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_strasse.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if(streetNumber.isEmpty()){
+
+                var bac: GradientDrawable = editText_hausnummer.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_hausnummer.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if(zipCode.isEmpty()){
+
+                var bac: GradientDrawable = editText_plz.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_plz.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if(city.isEmpty()){
+
+                var bac: GradientDrawable = editText_stadt.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_stadt.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if(phoneNumber.isEmpty()){
+
+                var bac: GradientDrawable = editText_telefone.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_telefone.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
             if (firstName.isEmpty() or secondName.isEmpty() or street.isEmpty() or streetNumber.isEmpty() or zipCode.isEmpty() or city.isEmpty() or phoneNumber.isEmpty()) {
                 Toast.makeText(activity, "Bitte Geben Sie alle Ihre Daten ein.", Toast.LENGTH_SHORT)
                     .show()
                 return@setOnClickListener
             }
+
+
 
             ServicesModule.localCustomerPersonalDataService.save(
                 CustomerPersonalData(
