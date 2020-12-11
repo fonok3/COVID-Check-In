7375b1788b1fb4b86eebd6e5fb015b385b58657b
Anmar Nazar Hizber <anmar_nh@outlook.com>
2020-12-11 21:04:20 +0100

    Like the last commit but for Resturant Flavor

--- app/src/restaurant/java/de/uni/hannover/hci/mi/team6/covidcheckin/contactForm/ui/ContactFormFragment.kt
+++ app/src/restaurant/java/de/uni/hannover/hci/mi/team6/covidcheckin/contactForm/ui/ContactFormFragment.kt
@@ -1,13 +1,14 @@
 package de.uni.hannover.hci.mi.team6.covidcheckin.contactForm.ui
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.google.firebase.FirebaseApp
 import de.uni.hannover.hci.mi.team6.covidcheckin.R
 import de.uni.hannover.hci.mi.team6.covidcheckin.bluetooth.BluetoothActivity
 import de.uni.hannover.hci.mi.team6.covidcheckin.model.Address
@@ -43,6 +44,7 @@ class ContactFormFragment : Fragment() {
 
 
         weiter_button.setOnClickListener {
+            val resturantName = this.editText_resturantname.text.toString().trim()
             val firstName = this.editText_Vorname.text.toString().trim()
             val secondName = this.editText_Nachname.text.toString().trim()
             val street = this.editText_strasse.text.toString().trim()
@@ -54,8 +56,104 @@ class ContactFormFragment : Fragment() {
             /**
              * Returns if not all Data were given
              */
+            if (resturantName.isEmpty()){
 
-            if (firstName.isEmpty() or secondName.isEmpty() or street.isEmpty() or streetNumber.isEmpty() or zipCode.isEmpty() or city.isEmpty() or phoneNumber.isEmpty()) {
+                var bac: GradientDrawable = editText_resturantname.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_resturantname.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if (firstName.isEmpty()){
+
+                var bac: GradientDrawable = editText_Vorname.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_Vorname.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if (secondName.isEmpty()){
+
+
+                var bac: GradientDrawable = editText_Nachname.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_Nachname.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if(street.isEmpty()){
+
+                var bac: GradientDrawable = editText_strasse.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_strasse.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if(streetNumber.isEmpty()){
+
+                var bac: GradientDrawable = editText_hausnummer.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_hausnummer.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if(zipCode.isEmpty()){
+
+                var bac: GradientDrawable = editText_plz.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_plz.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if(city.isEmpty()){
+
+                var bac: GradientDrawable = editText_stadt.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_stadt.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if(phoneNumber.isEmpty()){
+
+                var bac: GradientDrawable = editText_telefone.background as GradientDrawable
+                bac.setStroke(2, Color.RED)
+                bac.setColor(getResources().getColor(R.color.editTextWarning))
+            }
+            else {
+                var bac: GradientDrawable = editText_telefone.background as GradientDrawable
+                bac.setStroke(2, Color.BLACK)
+                bac.setColor(getResources().getColor(R.color.editTextNormal))
+            }
+
+            if (resturantName.isEmpty() or firstName.isEmpty() or secondName.isEmpty() or street.isEmpty() or streetNumber.isEmpty() or zipCode.isEmpty() or city.isEmpty() or phoneNumber.isEmpty()) {
                 Toast.makeText(activity, "Bitte Geben Sie alle Ihre Daten ein.", Toast.LENGTH_SHORT)
                     .show()
                 return@setOnClickListener
